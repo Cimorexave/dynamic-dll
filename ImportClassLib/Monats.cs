95999726d7c2320545004b732bcd61834c5d9b31
Cimorexave <sadeq.s502@gmail.com>
2023-06-16 16:43:13 +0330

    new test import classlib

---
+++ ImportClassLib/Monats.cs
@@ -0,0 +1,11 @@
+﻿namespace ImportClassLib;
+
+public class Monats
+{
+    public static void Convert()
+    {
+        Console.WriteLine("Converted (did nothing!)");
+    }
+    public static void SayHi() { Console.WriteLine("Hi!"); }
+    public static void PrintMessage(string message) { Console.WriteLine(message); }
+}
