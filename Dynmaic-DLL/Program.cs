e7c9dda2a037957fd30a39ca7e20ec4b4b2a6876
Cimorexave <sadeq.s502@gmail.com>
2023-06-16 16:38:24 +0330

    Add project files.

---
+++ Dynmaic-DLL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+var input = String.Empty;
+bool inputIsValid()
+{
+    try
+    {
+        string path = Path.GetFullPath(input ?? "");
+        if (!File.Exists(path)) return false;
+        return true;
+    }
+    catch
+    {
+        return false;
+    }
+}
+while (!inputIsValid())
+{
+    Console.Write("Enter the address of the dll file: ");
+    input = Console.ReadLine();
+}
+Console.WriteLine(input);
+Assembly assembly = Assembly.LoadFile(path: input ?? "");
+Type type = assembly?.GetType("Monatsjournal.Monatsjournal")!;
+MethodInfo method = type?.GetMethod("Convert")!;
+Console.WriteLine($"assembly: {assembly} class: {type} method: {method}");
+_ = method?.Invoke(null, null);
+Console.ReadLine();
