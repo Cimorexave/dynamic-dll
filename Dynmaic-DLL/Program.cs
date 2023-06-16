c1b0fb3c1a11115ee10bf833f2ca96c18a5eed19
Cimorexave <sadeq.s502@gmail.com>
2023-06-16 16:46:39 +0330

    new build; changed in sayhi method msg

--- Dynmaic-DLL/Program.cs
+++ Dynmaic-DLL/Program.cs
@@ -23,7 +23,7 @@ Console.WriteLine(input);
 Assembly assembly = Assembly.LoadFile(path: input ?? "");
 //Type type = assembly?.GetType("Monatsjournal.Monatsjournal")!;
 Type type = assembly?.GetType("ImportClassLib.Monats")!;
-MethodInfo method = type?.GetMethod("Convert")!;
+MethodInfo method = type?.GetMethod("SayHi")!;
 Console.WriteLine($"assembly: {assembly} class: {type} method: {method}");
 _ = method?.Invoke(null, null);
 Console.ReadLine();
